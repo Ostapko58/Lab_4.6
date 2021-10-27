d7d7d2f62b43ed9fba60ac3059bbe1bb70c4d4d8
Ostap Onyshkiv <prostoostap58@gmail.com>
2021-10-27 20:26:22 +0300

    add files

---
+++ Lab_4.6/Lab_4.6.cpp
@@ -0,0 +1,92 @@
+// Lab_4.6.cpp
+// < Онишківа Остапа >
+// Лабораторна робота No 4.6
+// Вкладені цикли.
+// Варіант 5
+
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int i, k;
+	double S, P;
+	i = 1;
+	P = 1;
+
+	while (i<=10)
+	{
+		S = 0;
+		k = 1;
+
+		while (k<=i)
+		{
+			S += 1 / k;
+			k++;
+		}
+
+		P *= (1 + S) / sqrt(S);
+		
+		i++;
+	}
+
+	cout << P << endl;
+	
+	i = 1;
+	P = 1;
+
+	do
+	{
+		S = 0;
+		k = 1;
+		
+		do
+		{
+			S += 1 / k;
+			
+			k++;
+		} while (k <= i);
+		
+		P *= (1 + S) / sqrt(S);
+		
+		i++;
+	} while (i <= 10);
+	
+	cout << P << endl;
+
+	P = 1;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		S = 0;
+		
+		for (int k = 1; k <= i; k++)
+		{
+			S += 1 / k;
+		}
+		
+		P *= (1 + S) / sqrt(S);
+	}
+	
+	cout << P << endl;
+
+	P = 1;
+
+	for (int i = 10; i >=1; i--)
+	{
+		S = 0;
+		
+		for (int k = i; k >=1; k--)
+		{
+			S += 1 / k;
+		}
+		
+		P *= (1 + S) / sqrt(S);
+	}
+	
+	cout << P << endl;
+
+	return 0;
+}
